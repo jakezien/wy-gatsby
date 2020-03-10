a744ca27c864802e2df68dd2ee497f15c5df8b32
Jake Zien <jake@floored.com>
2020-03-10 19:14:38 -0400

    Typography updates and schools page

--- src/components/nextButton.js
+++ src/components/nextButton.js
@@ -2,14 +2,25 @@ import React from "react"
 import { Link } from "gatsby"
 import { rhythm, scale } from "../utils/typography"
 import Container from "./container"
+import styled from "styled-components"
+
+const StyledH1 = styled.h1`
+  margin-top: -1rem;
+  font-size: 3.5em;
+  letter-spacing: -0.03em;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
 
 const NextButton = ({ title, caption, uri }) => {
   return (      
     <Container>
-      <Link to={uri}>
+      <StyledLink to={uri}>
         <p>Next: <h4 style={{display: "inline"}}>{caption}</h4></p>
-        <h1 style={{marginTop: "-1rem"}}>{title}</h1>
-      </Link>
+        <StyledH1>{title}</StyledH1>
+      </StyledLink>
     </Container>
   )  
 }
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,44 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
+const typography = new Typography({
+  baseFontSize: "22px",
+  baseLineHeight: 1.4,
+  scaleRatio: 2,
+  googleFonts: [
+    {
+      name: 'EB Garamond',
+      styles: [
+        '300',
+        '300i',
+        '400',
+        '400i',
+        '700',
+        '700i',
+      ],
     },
-  }
-}
+  ],
+  headerFontFamily: [
+    "EB Garamond",
+    "Garamond",
+    "serif",
+  ],
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+  bodyFontFamily: [
+    "EB Garamond",
+    "Garamond",
+    "serif",
+  ],
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
+    'h1': {
+      fontSize: '6em',
+      fontWeight: '300',
+      letterSpacing: '-.05em'
+    },
+    'h3': {
+      fontWeight: 500
+    }
+  })
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
