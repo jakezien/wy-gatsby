2b3134990334a1ff21e0ef0763718027ad115dc2
jakezien <jakezien@hey.com>
2022-03-22 14:11:02 -0400

    ctabutton progress

--- src/components/ctaButton.js
+++ src/components/ctaButton.js
@@ -7,7 +7,7 @@ const CtaButton = ({ to, text, caption, next, className }) => {
     <Link to={to} className={`${className} no-underline`}>
       {next && <span>Next: </span>}
       <h4 style={{display: "inline"}}>{caption}</h4>
-      <h1 className="-mt-1">{text}</h1>
+      <h1 className="my-2 py-4 border border-x-0 border-y-[1]">{text}</h1>
     </Link>
 
   )  
