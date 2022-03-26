e0446ddeaa926834dade1a433cc27bd16ac9f4c1
jakezien <jakezien@hey.com>
2022-03-25 20:25:24 -0400

    header work + add top offset to coverPhoto

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,18 +4,18 @@ import Logotype from "./logotype"
 
 const Nav = () => {
   return (
-    <nav className="px-1 py-0.5 fixed z-40 w-full">
-      <ul className="w-full flex justify-between list-none font-franklin uppercase tracking-widest font-normal">
-        <li className="mx-4 self-start"><Link to="/"><Logotype small className="fill-black w-12 h-9" /></Link></li>
-        <div className="flex justify-center">
-          <li className="mx-4"><Link to="/expeditions">Expeditions</Link></li>
-          <li className="mx-4"><Link to="/qeros">Q’eros</Link></li>
-          <li className="mx-4"><Link to="/projects">Projects</Link></li>
-          <li className="mx-4"><Link to="/schools">Schools</Link></li>
-          <li className="mx-4"><Link to="/journal">Journal</Link></li>
-          <li className="mx-4"><Link to="/about">About</Link></li>
+    <nav className="px-1 py-0.5 fixed z-40 w-full bg-cloud">
+      <ul className="w-full flex justify-between items-center list-none font-franklin uppercase tracking-[0.2em] font-normal">
+        <li className="mx-4 my-2 self-start"><Link to="/"><Logotype small className="fill-black w-11 h-8" /></Link></li>
+        <div className="flex justify-center items-center">
+          <li className="mx-4 my-0"><Link to="/expeditions">Expeditions</Link></li>
+          <li className="mx-4 my-0"><Link to="/qeros">Q’eros</Link></li>
+          <li className="mx-4 my-0"><Link to="/projects">Projects</Link></li>
+          <li className="mx-4 my-0"><Link to="/schools">Schools</Link></li>
+          <li className="mx-4 my-0"><Link to="/journal">Journal</Link></li>
+          <li className="mx-4 my-0"><Link to="/about">About</Link></li>
         </div>
-        <li className="mx-4 self-end"><Link to="/donate">Donate</Link></li>
+        <li className="mx-4 my-0 border rounded-sm border-darkBrown"><Link to="/donate" className="block px-3 py-1">Donate</Link></li>
       </ul>
     </nav>
   )
