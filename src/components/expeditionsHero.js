91c9ed9850d2a8c3844f065429cbb5f1e9ed7c04
jakezien <jakezien@hey.com>
2022-04-19 16:00:17 -0400

    more expeditions work

--- src/components/expeditionsHero.js
+++ src/components/expeditionsHero.js
@@ -17,19 +17,21 @@ const ExpeditionsHero = (props) => {
       className="relative h-full"
       layers={[
       {
+        // sky
         speed: 0,
         style: { position: 'fixed', inset:'none !important' },
         className: '-z-1',
         children: (
           <StaticImage
             alt=""
-            src='../images/expeditions/sky@2x.jpg'
-            className="w-full h-full bg-cover fixed object-bottom"
+            src='../images/expeditions/sky2x.jpg'
+            className="w-full h-full md:bg-cover fixed object-bottom"
             layout="fullWidth"
           />
         )
       },
       {
+        // title  
         speed: -50,
         children: (
           <div className="absolute w-full h-full flex flex-col justify-center">
@@ -39,42 +41,42 @@ const ExpeditionsHero = (props) => {
         )
       },
       {
+        // mountains
         speed: -10,
         children: (
           <StaticImage 
             alt="" 
-            src='../images/expeditions/top@2x.jpg'
-            className="w-full absolute left-0 bottom-0 object-bottom" 
+            src='../images/expeditions/top2x.jpg'
+            className="w-full absolute left-0 right-0 bottom-[10vh] sm:bottom-[3%] object-bottom" 
             style={{
               maskImage: 'url(mountains.svg)',
               maskPosition: 'center 67%',
               maskRepeat: 'no-repeat',
               maskSize: '100% auto',
-              bottom: '3%'
             }}/>
         ) 
       },
       {
+        // tents
         speed: 5,
         children: (
           <StaticImage
-          alt="A starry sky framed by mountains. At their base, several tents glow from lantern light within."
-          src='../images/expeditions/top@2x.jpg'
-          className="w-full absolute left-0 bottom-0 object-bottom"
-          style={{
-            maskImage: 'url(tents.svg)',
-            maskPosition: 'center bottom',
-            maskRepeat: 'no-repeat',
-            maskSize: '100% auto',
-            bottom: '0%'
-          }}/>
+            alt="A starry sky framed by mountains. At their base, several tents glow from lantern light within."
+            src='../images/expeditions/top2x.jpg'
+            className="w-full absolute left-0 right-0 bottom-[3vh] object-bottom"
+            style={{
+              maskImage: 'url(tents.svg)',
+              maskPosition: 'center bottom',
+              maskRepeat: 'no-repeat',
+              maskSize: '100% auto',
+            }}/>
           )
         },
         {
+          // subtitle
           speed: -20,
           opacity: [1,0],
           startScroll: 0,
-          // shouldAlwaysCompleteAnimation: true,
           endScroll: 350,
           children: (
               <div className="absolute w-full h-full flex flex-col justify-center top-[256px]">
